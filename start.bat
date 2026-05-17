ded380214680e7690addb9675df3d8aecdc92185
FinVisTech <finvision24@gmail.com>
2026-05-17 02:47:37 -0700

    feat: hide dev tools behind ?dev URL parameter

--- start.bat
+++ start.bat
@@ -6,10 +6,10 @@ echo ========================================
 echo.
 
 :: Open browser after a short delay (gives server time to start)
-start "" cmd /c "timeout /t 2 /nobreak >nul && start http://127.0.0.1:8080"
+start "" cmd /c "timeout /t 2 /nobreak >nul && start http://127.0.0.1:8080/?dev"
 
 :: Start the server (this blocks until Ctrl+C)
-echo Server running at http://127.0.0.1:8080
+echo Server running at http://127.0.0.1:8080/?dev
 echo Press Ctrl+C to stop.
 echo.
 npx.cmd -y http-server . --port 8080 -c-1
